6ce2de824bc8fb3ce0770789b504110d002f827b
Jonas Frei <jonas.frei@ost.ch>
2022-06-02 14:03:30 +0200

    add doxy

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * @brief main function
+ * 
+ * @param argc test
+ * @param argv test2
+ * @return int 0
+ */
+int main (int argc,int ** argv)
+{
+    printf("test\r\n");
+    return 0;
+}
